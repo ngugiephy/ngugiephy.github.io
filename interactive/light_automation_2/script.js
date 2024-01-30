3754039d2b699d48e46e7c8cad688a74c1b61fc1
ngugiephy <ngugiephy@gmail.com>
2024-01-30 05:21:04 +0300

    fixing a bug with the 'enter scene' button

--- interactive/light_automation_2/script.js
+++ interactive/light_automation_2/script.js
@@ -2,7 +2,7 @@ $(window).on('load', function() {
   // Animate loader off screen
   // $("#splash_screen").delay(5000).fadeOut("slow");;
   // alert("adsf");
-  $('#op').delay(10000).html('<div class="animate__animated animate__bounce" style="color:#00FF49;background-color: #616161;box-shadow: 0px 0px 8px #888888;padding:5px;margin-bottom:10px;margin-top:100px;">Content Loaded <i class="fa fa-check"></i></div><button type="button" class="btn btn-outline-success btn-lg" id="enter" style="box-shadow: 0px 0px 8px #888888;">Enter Scene</button>');
+  $('#op').html('<div class="animate__animated animate__bounce" style="color:#00FF49;background-color: #616161;box-shadow: 0px 0px 8px #888888;padding:5px;margin-bottom:10px;margin-top:100px;">Content Loaded <i class="fa fa-check"></i></div><button type="button" class="btn btn-outline-success btn-lg" id="enter" style="box-shadow: 0px 0px 8px #888888;">Enter Scene</button>');
 
 });
 
@@ -14,7 +14,11 @@ $( document ).ready(function() {
     });
 });
 
-
+$('#enter').click(function(){
+      $("#splash_screen").fadeOut("slow");
+      
+      return false;
+    });
 
 
 
